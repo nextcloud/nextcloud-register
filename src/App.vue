59f0e2296607b759d9d4aebea56a75a07355f7ba
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-04-26 14:55:17 +0200

    Bump design version 1

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,133 @@
 <template>
-  <div>
-    {{this.providers}}
-  </div>
+	<div id="register" class="container revealOnScroll">
+		<provider :provider="selected" :show="true" class="selected-provider revealOnScroll" />
+		<div id="show-more" class="button--dropdown revealOnScroll"
+			 :class="{opened: showAll}" @click="toggleShowAll"><span>{{showAll ? 'close' : 'change provider'}}</span></div>
+		<form id="form" class="revealOnScroll" @submit.prevent="register" ref="register">
+			<div class="email">
+				<input type="email" required value="" placeholder="Your email address" id="emailprovider" />
+				<input type="submit" class="btn btn-primary" value="Sign Up" />
+			</div>
+			<div class="newsletter"></div>
+		</form>
+		<div id="providers" v-if="showAll===true">
+			<provider v-for="(provider, key) in providers" :key="key" :provider="provider" />
+		</div>
+	</div>
 </template>
 
 <script>
-import Vue from "vue";
-import provider from "./Components/Provider";
+import provider from './Components/Provider';
+import VueScrollTo from 'vue-scrollto';
 
 export default {
-	name: "app",
+	name: 'app',
 	components: {
 		provider
 	},
-	mounted() {
-		this.sortProvider(48.258101, 9.164655);
+	data() {
+		return {
+			// Default nextcloud location
+			ll: [48.7871141, 9.1547062],
+			selected: false,
+			showAll: false
+		};
+	},
+	beforeMount() {
+		let location = JSON.parse(window.register.dataset.ll);
+		if (location.latitude && location.longitude) {
+			//this.ll = [location.latitude, location.longitude];
+		}
+		this.scoreProvider(this.ll[0], this.ll[1]);
 	},
 	computed: {
 		providers() {
 			return [
 				{
-					name: "Provider 1",
+					name: 'Provider 1',
+					details:
+						'This is a sample text to describe this provider. We deeply care about our customers and there data.',
 					ll: [49.1972, -0.3268],
-					city: "Caen",
-					country: "France",
-					apps: ["core", "contacts", "calendar", "spreed", "mail"]
+					city: 'Caen',
+					country: 'France',
+					countryCode: 'fr',
+					apps: ['core', 'contacts', 'calendar', 'spreed', 'mail']
 				},
 				{
-					name: "Provider 2",
+					name: 'Provider 2',
+					details:
+						'This is a sample text to describe this provider. We deeply care about our customers and there data.',
 					ll: [48.8567, 2.3508],
-					city: "Paris",
-					country: "France",
-					apps: ["core", "contacts", "calendar"]
+					city: 'Paris',
+					country: 'France',
+					countryCode: 'fr',
+					apps: ['core', 'contacts', 'calendar']
 				},
 				{
-					name: "Provider 3",
+					name: 'Provider 3',
+					details:
+						'This is a sample text to describe this provider. We deeply care about our customers and there data.',
 					ll: [47.198983, -1.445317],
-					city: "Nantes",
-					country: "France",
-					apps: ["core", "contacts", "calendar", "mail"]
+					city: 'Nantes',
+					country: 'France',
+					countryCode: 'fr',
+					apps: ['core', 'contacts', 'calendar', 'mail']
+				},
+				{
+					name: 'Provider 2',
+					details:
+						'This is a sample text to describe this provider. We deeply care about our customers and there data.',
+					ll: [48.8567, 2.3508],
+					city: 'Paris',
+					country: 'France',
+					countryCode: 'fr',
+					apps: ['core', 'contacts', 'calendar']
+				},
+				{
+					name: 'Provider 3',
+					details:
+						'This is a sample text to describe this provider. We deeply care about our customers and there data.',
+					ll: [47.198983, -1.445317],
+					city: 'Nantes',
+					country: 'France',
+					countryCode: 'fr',
+					apps: ['core', 'contacts', 'calendar', 'mail']
+				},
+				{
+					name: 'Provider 2',
+					details:
+						'This is a sample text to describe this provider. We deeply care about our customers and there data.',
+					ll: [48.8567, 2.3508],
+					city: 'Paris',
+					country: 'France',
+					countryCode: 'fr',
+					apps: ['core', 'contacts', 'calendar']
+				},
+				{
+					name: 'Provider 3',
+					details:
+						'This is a sample text to describe this provider. We deeply care about our customers and there data.',
+					ll: [47.198983, -1.445317],
+					city: 'Nantes',
+					country: 'France',
+					countryCode: 'fr',
+					apps: ['core', 'contacts', 'calendar', 'mail']
 				}
 			];
 		}
 	},
 	methods: {
+		// submit
+		register(event) {
+			return false;
+		},
+		// toggle showAll
+		toggleShowAll() {
+			this.showAll = !this.showAll;
+			VueScrollTo.scrollTo(!this.showAll ? '#register' : '#form', 500, {
+				offset: -100
+			});
+		},
 		// convert deg to rad
 		deg2Rad(deg) {
 			return deg * Math.PI / 180;
@@ -63,7 +147,7 @@ export default {
 			return d;
 		},
 		// add distance to provider
-		sortProvider(latitude, longitude) {
+		scoreProvider(latitude, longitude) {
 			let mindif = 99999;
 			this.providers.forEach((provider, index) => {
 				let dif = this.pythagorasEquirectangular(
@@ -72,43 +156,103 @@ export default {
 					provider.ll[0],
 					provider.ll[1]
 				);
-				Vue.set(provider, "score", Math.round(dif));
+				if (dif < mindif) {
+					mindif = dif;
+					this.selected = provider;
+				}
+				this.$set(provider, 'score', Math.round(dif));
 			});
-			this.providers.sort((a, b) => {
-				return a.score - b.score;
-			});
-			console.log(this.providers);
 		}
 	}
 };
 </script>
 
-<style lang="scss">
-#app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-
-h1,
-h2 {
-	font-weight: normal;
-}
-
-ul {
-	list-style-type: none;
+<style lang="scss" scoped>
+#register {
+	width: 500px;
+	max-width: 100vw;
+	position: relative;
+	margin-top: -195px;
 	padding: 0;
 }
-
-li {
-	display: inline-block;
-	margin: 0 10px;
+#providers {
+	display: flex;
+	flex-direction: column;
+	max-width: 500px;
+	margin: auto;
+	margin-top: 20px;
 }
-
-a {
-	color: #42b983;
+#show-more {
+	$height: 20px;
+	height: $height * 2 + 10px;
+	width: 100%;
+	text-align: center;
+	font-weight: 100;
+	cursor: pointer;
+	position: relative;
+	opacity: 1;
+	box-sizing: content-box;
+	color: #fff;
+	span {
+		height: $height;
+		transition: all 0.2s ease-in;
+		display: block;
+		line-height: $height;
+	}
+	&:hover::before {
+		top: $height + 2px;
+	}
+	&::before {
+		transition: all 0.2s ease-in;
+		right: 50%;
+		margin-right: -10px;
+		top: $height;
+		height: $height;
+	}
+	&.opened {
+		span {
+			padding-top: $height;
+		}
+		&::before {
+			transform: scaleY(-1);
+			top: 0;
+		}
+	}
+}
+#form {
+	top: 30vh;
+	width: 100%;
+	padding: 0 10px;
+}
+.email {
+	display: flex;
+	align-items: center;
+	filter: drop-shadow(0 2px 5px rgba(0, 0, 0, 0.4));
+	> input {
+		&:focus {
+			outline: none;
+		}
+		&[type='email'] {
+			height: 44px;
+			padding: 5px 35px 5px 20px;
+			font-size: 18px;
+			width: 100%;
+			border-width: 0px;
+			border-radius: 22px 0 0 22px;
+		}
+		&[type='submit'] {
+			background-image: url('/wp-content/themes/nextcloud.com/assets/img/arrow_right.svg');
+			background-position: calc(100% - 20px) center;
+			background-repeat: no-repeat;
+		}
+	}
+	> .btn-primary {
+		padding: 10px 60px 10px 20px;
+		height: 44px;
+		margin-left: -25px;
+		&:hover {
+			background-position: calc(100% - 10px) center;
+		}
+	}
 }
 </style>
--- src/Components/Provider.vue
+++ src/Components/Provider.vue
@@ -1,3 +1,148 @@
 <template>
-	<div></div>
+	<!-- do not show selected -->
+	<div class="provider" :style="{order:order}"
+		 @click="selectProvider" v-if="selected !== provider || show">
+		<h3>{{provider.name}}</h3>
+		<p class="summary">{{provider.details}}</p>
+		<div class="location">
+			<span class="city-country">{{provider.city}}
+				<img :alt="provider.country" :title="provider.country"
+					 :src="'/wp-content/themes/nextcloud.com/assets/img/flags/'+provider.countryCode+'.gif'"/>
+			</span>
+			<span class="distance">{{distance}}</span>
+		</div>
+		<div class="apps">
+			<!-- core is un-removable -->
+			<img alt="Files" height="16" width="16"
+				 src="/wp-content/themes/nextcloud.com/assets/img/apps/core.svg"/>
+			<img v-for="(app, key) in provider.apps" :key="key"
+				 :alt="app" height="16" width="16" v-if="officialApps.indexOf(app)>=0"
+				 :src="'/wp-content/themes/nextcloud.com/assets/img/apps/'+app+'.svg'"/>
+		</div>
+	</div>
 </template>
+<script>
+import VueScrollTo from 'vue-scrollto';
+
+export default {
+	name: 'provider',
+	props: ['provider', 'show'],
+	methods: {},
+	data() {
+		return {
+			officialApps: ['contacts', 'calendar', 'spreed', 'mail', 'tasks']
+		};
+	},
+	computed: {
+		distance() {
+			if (
+				this.provider.score <= 500 &&
+				typeof this.provider.score === 'number'
+			) {
+				return this.provider.score + 'km';
+			} else {
+				return 'Far far away';
+			}
+		},
+		selected: {
+			get() {
+				return this.$parent.selected;
+			},
+			set(provider) {
+				this.$parent.selected = provider;
+			}
+		},
+		showAll: {
+			get() {
+				return this.$parent.showAll;
+			},
+			set(showAll) {
+				this.$parent.showAll = showAll;
+			}
+		},
+		order() {
+			// selected is first
+			if (this.selected === this.provider) {
+				return -1;
+			}
+			return this.provider.score;
+		}
+	},
+	methods: {
+		selectProvider() {
+			this.selected = this.provider;
+			this.showAll = !this.showAll;
+			VueScrollTo.scrollTo('#register', 500, {offset: -50});
+			window.emailprovider.focus()
+		}
+	}
+};
+</script>
+
+<style lang='scss' scoped>
+.provider {
+	background: #fff;
+	margin: 10px;
+	padding: 10px;
+	position: relative;
+	padding-right: 110px;
+	// override display block after animation
+	display: flex !important;
+	flex-wrap: wrap;
+	flex-shrink: 0;
+	border-radius: 5px;
+	user-select: none;
+	flex-direction: column;
+	transition: all 0.2s ease-in;
+	&:not(.selected-provider) {
+		cursor: pointer;
+		box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.4);
+		&:hover {
+			box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.5);
+		}
+	}
+	h3 {
+		margin: 0;
+		flex-grow: 1;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		font-size: 22px;
+	}
+	p.summary {
+		font-weight: 100;
+		opacity: 0.9;
+		line-height: 1.2em;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		text-align: justify;
+		margin-top: 0;
+	}
+	.location {
+		display: flex;
+		flex-direction: column;
+		text-align: right;
+		opacity: 0.7;
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		.city-country {
+			display: flex;
+			align-items: center;
+			justify-content: flex-end;
+			img {
+				margin-left: 5px;
+			}
+		}
+	}
+	.apps {
+		flex: 0 0 100%;
+		display: flex;
+		img {
+			margin-right: 5px;
+			opacity: 0.5;
+		}
+	}
+}
+</style>
